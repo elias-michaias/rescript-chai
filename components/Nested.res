fd98ab5f032d6fa7e1be7c9e19bbb990d77c0890
Elias Michaias <emskeirik@gmail.com>
2025-10-22 14:18:00 -0400

    major project restructuring

--- components/Nested.res
+++
@@ -1,9 +0,0 @@
-@react.component
-let make = () => {
-    <div>
-        <h1>{React.string("Nested Component")}</h1>
-        <Counter.make count=20 />
-    </div>
-}
-
-let default = make
--- examples/counter/vite.config.js
+++ examples/counter/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import rescript from "@jihchi/vite-plugin-rescript";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    rescript()
+    tailwindcss(),
+    react({
+      include: ["**/*.res.mjs"],
+    }),
   ],
 });
-
